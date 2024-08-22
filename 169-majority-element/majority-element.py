8b60dba34ee85f66120863a94f32f3fc3f3381b0
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-08-22 17:04:42 -0500

    Time: 151 ms (89.92%) | Memory: 13.3 MB (65.66%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        nums.sort()
+        return nums[n/2]
+              
