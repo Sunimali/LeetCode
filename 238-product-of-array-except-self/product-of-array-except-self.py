7fddf43b76482a78bb8e3d375e29ceff27e6b4f9
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-09-17 23:18:45 -0500

    Time: 499 ms (7.34%) | Memory: 18.6 MB (90.74%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,26 @@
+import copy
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        size = len(nums)
+        postNum = copy.copy(nums)
+
+        for i in range(size-1,-1,-1):
+            # print(i)
+            if(i==size-1):
+                postNum[i] = nums[i]
+            else:
+                postNum[i] = nums[i]* postNum[i+1]
+        preMul = 1
+        for i in range(size):
+            if(i==size-1):
+                nums[i] = preMul
+            else:    
+                temp =  preMul * nums[i]
+                nums[i] = preMul * postNum[i+1]
+                preMul = temp
+
+        return nums
