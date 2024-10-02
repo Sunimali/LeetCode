e2ea08a571441555caf29d581275ceed92ec1911
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-10-02 17:00:28 -0500

    Time: 38 ms (53.66%) | Memory: 11.8 MB (76.21%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -7,39 +7,31 @@ class Solution(object):
         """
         size = len(s)
         str = ""
-        first = (numRows + (numRows - 1) )-1
-        second = 0
+        first = (numRows + (numRows - 1) )-1 #find the start iterator
+        second = 0 #find the second iterator
 
-        if size == 1 or numRows ==1:
+        if size == 1 or numRows ==1: #retrun string
             return s
         
-        for i in range(numRows-1,-1,-1):
-            print("row")
-            print(i)
-            print(first)
-            print(second)
-            
-            start = numRows - 1 - i
-            shift = 1
+        for i in range(numRows-1,-1,-1): #iterate through rows
+            start = numRows - 1 - i #find intial for each row
+            shift = 1 #whether need to shift iterater from first to one
             current = start
-            while current < size:
-                print("current")
-                print(current)
-                print(s[current])
+            while current < size: #iterate for all characters
                 str = str + s[current]
-                if(second == 0):
+                if(second == 0): #if one iterator is zero keep using none zero iterator without shifting
                     current = current + first
                 elif(first == 0):
                     current = current + second
-                else:
+                else: #else shift iterator after one turn
                     if(shift):
-                        current = current + first
+                        current = current + first #update current position based on selected iterator
                         shift = 0
                     else:
                         current = current + second
                         shift = 1
 
-            first = first - 2
+            first = first - 2 #update the iterator
             second = second + 2        
         return str
                
