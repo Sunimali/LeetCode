d282d60c5695098ab37949e0981e75f98b3a45e2
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-10-29 16:34:36 -0500

    Time: 32 ms (67.81%) | Memory: 18.7 MB (61.37%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -10,17 +10,32 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        dict_visited = {}
+        # dict_visited = {}
 
-        current = head
+        # current = head
         
 
-        while current is not None:
-            if current in dict_visited: # check nodes in dictionary or not
-                return True #ther is a loop
-            else:    
-                dict_visited[current] = True #else add to dictionary
-                current = current.next #move to next element
-        return False  #if last element doesnt have  pointer thne it is none      
+        # while current is not None:
+        #     if current in dict_visited: # check nodes in dictionary or not
+        #         return True #ther is a loop
+        #     else:    
+        #         dict_visited[current] = True #else add to dictionary
+        #         current = current.next #move to next element
+        # return False  #if last element doesnt have  pointer thne it is none   
+
+        #lets try less momry need approach
+        
+        slow = head
+        if(head is None or head.next is None) :
+            return False
+        fast = head.next.next #iterate faster by 2 steps
+
+        while fast is not None and fast.next is not None: #fast one hit final:mean no cycle
+            if(slow==fast): #if cycle apear to be then two gets ovelapped
+                return True
+            slow = slow.next #iterate
+            fast = fast.next.next
+
+        return False      
 
         
