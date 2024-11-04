12a4f690b7eef9c319c17d1d05f786a1ccfc6625
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-11-03 21:33:18 -0600

    Time: 0 ms (100.00%) | Memory: 11.6 MB (86.77%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -9,39 +9,67 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
-        prev = None
-        current = head
-        if head is None: #emply list
-            return head
-        head_dup = 0 #check whether head is duplicated or no
+    #without dummy node   
+        # prev = None
+        # current = head
+        # if head is None: #emply list
+        #     return head
+        # head_dup = 0 #check whether head is duplicated or no
 
-        while current is not None: #iterate through whole list
-            if(prev is None): #handle first node iteration
+        # while current is not None: #iterate through whole list
+        #     if(prev is None): #handle first node iteration
+        #         prev = current
+        #         current = current.next
+        #     else:
+        #         initial = current
+        #         init_prev = prev #first occuorance of a node values
+        #         count = 0 #keep occurance count
+        #         while( current is not None and current.val == initial.val): #iterate same val
+        #             if(current.val == head.val): #head has duplicates
+        #                 head_dup = 1 #set head has duplicates
+                   
+        #             prev = current
+        #             current = current.next
+        #             count = count + 1 #increase the occurence count
+        #         if(head_dup ): #if head is dupllicates change the head to new after removing
+        #             head = current
+        #             prev = None
+        #             head_dup = 0 #reset flag
+        #             continue
+            
+        #         #now current is not equal to initial
+        #         #1 is not having same occurence since initail and current was same pointer    in the begining
+        #         if count > 1: #Found more than once ocurence
+        #             init_prev.next = current 
+        #             prev = init_prev
+        # return head
+
+        #with dummy node
+
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev = dummy #prev node has initial value rather than noe
+        current = head
+
+        while current is not None: #no need to handle prev none sperately
+            initial = current
+            init_prev = prev #first occuorance of a node values
+            count = 0
+
+            while(current is not None and current.val == initial.val):
                 prev = current
                 current = current.next
-            else:
-                initial = current
-                init_prev = prev #first occuorance of a node values
-                count = 0 #keep occurance count
-                while( current is not None and current.val == initial.val): #iterate same val
-                    if(current.val == head.val): #head has duplicates
-                        head_dup = 1 #set head has duplicates
-                   
-                    prev = current
-                    current = current.next
-                    count = count + 1 #increase the occurence count
-                if(head_dup ): #if head is dupllicates change the head to new after removing
-                    head = current
-                    prev = None
-                    head_dup = 0 #reset flag
-                    continue
-            
-                #now current is not equal to initial
-                #1 is not having same occurence since initail and current was same pointer    in the begining
-                if count > 1: #Found more than once ocurence
-                    init_prev.next = current 
-                    prev = init_prev
-        return head
+                count = count + 1 #increase the occurence count
+
+            if count > 1:
+                init_prev.next = current
+                prev = init_prev
+        return dummy.next
+
+
+
+
+
 
    
