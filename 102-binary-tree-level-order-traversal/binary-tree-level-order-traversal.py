a59cc76558a1a1ad1edecc86c19a9e5d2e5efa00
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-11-08 10:34:56 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (87.66%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -10,7 +10,6 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-
         queue = []
         answer = []
 
@@ -27,47 +26,4 @@ class Solution(object):
                     queue.append(t.right)  
             if ans:
                 answer.append(ans)
-
         return answer    
-
-
-        #iterative method 
-
-        # h = self.depth(root)
-        # answer = []
-        # for i in range(1,h+1):
-        #     ans = []
-        #     self.traversal(root,i, ans)
-        #     answer.append(ans)
-        # return answer
-
-    #     level = 0
-    #     ans = []
-    #     self.traverse(root,level,ans)
-    #     return ans
-
-    # def traverse(self, root, level, ans):
-    #     if root is None:
-    #         return
-    #     if level == len(ans):
-    #         ans.append([])
-    #     ans[level].append(root.val)
-    #     self.traverse(root.left, level+1, ans)
-    #     self.traverse(root.right, level+1,ans) 
-            
-  
-
-    # def traversal(self, root, level, ans):
-    #     if root is None:
-    #         return
-    #     elif level == 1:
-    #         ans.append(root.val)
-    #     else:
-    #         self.traversal(root.left, level-1, ans)
-    #         self.traversal(root.right, level-1, ans)                
-
-    # def depth(self,root):
-    #     if root is None:
-    #         return 0
-    #     else:
-    #         return 1+ max(self.depth(root.left), self.depth(root.right))       
