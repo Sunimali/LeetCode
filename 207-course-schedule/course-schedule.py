27712adee273442c21b1460f34bb16ee5d799a96
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2024-11-19 16:08:04 -0600

    Time: 2450 ms (5.10%) | Memory: 13.8 MB (62.13%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+
+        #build the graph
+
+        p = len(prerequisites)
+        graph = {}
+
+        for i in range(p):
+            c1 = prerequisites[i][0] #first course
+            c2 = prerequisites[i][1] #second course
+
+            if c2 in graph:
+                graph[c2].append(c1)
+            else:
+                graph[c2] = []
+                graph[c2].append(c1)
+ 
+        #traverse dfs graph and find cycles exsist or not
+
+        for node in graph:
+            visited =  [False] *numCourses
+            rec_stack = [False] *numCourses
+            ans = self.dfs_search(graph, node,visited,rec_stack)
+            if not ans:
+                return False
+        return True        
+
+    def dfs_search(self,graph,node,visited,rec_stack):
+        visited[node] = True
+        rec_stack[node] = True
+        neighbours =  []
+        if node in graph:
+            neighbours = graph[node]
+
+
+        for n in neighbours:
+            if not visited[n]:
+                ans = self.dfs_search(graph,n,visited,rec_stack)
+                if not ans:
+                    return False
+            elif  rec_stack[n]:
+                return False
+        rec_stack[node] = False        
+        return True              
+
+
+        
+
+
+        
