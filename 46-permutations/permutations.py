76b8cc0e3485418da5c912a38e854f5b385d3f57
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2025-01-18 12:39:05 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (65.74%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,37 @@
+import copy
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results = []
+        count = 0
+
+        results = self.rec_permute(nums, [],  results )
+        return results
+        
+
+    def rec_permute(self, nums, current,results ):
+        if len(current) == len(nums):
+            
+            results.append(current[:])
+            
+            return results
+       
+        for item in nums:
+            if item in current:
+                continue
+            else:
+                current.append(item)
+                self.rec_permute(nums, current,results )
+                current.pop()
+                 
+        return results
+           
+
+        
+        
+
+
+        
