6bf1e36726e04ae1916305890aad6c0a1a836d5c
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2025-01-20 12:26:56 -0600

    Time: 13 ms (38.66%) | Memory: 14.6 MB (7.03%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,7 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        nums.sort()
-        return nums[n/2]
-              
+        dic = {}
+        size = len(nums)
+
+        if size == 1:
+            return nums[0]
+        if size ==0:
+            return 0
+       
+        maj = size//2
+        for i in range(size):
+            if nums[i] in dic:
+                dic[nums[i]] = dic[nums[i]] + 1
+                if maj < dic[nums[i]]:
+                    return nums[i]
+            else:
+                dic[nums[i]] = 1
+        return 0
+
+        
