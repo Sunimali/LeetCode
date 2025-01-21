a09203691f612a0eda41e79f7d9c2af03bf5c14f
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2025-01-21 16:18:31 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.22%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -2,19 +2,48 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
         size = len(nums)
-        if size ==0:
-            return None
         if size == 1:
             return 0
-        for i in range(size):
-            if i == 0: #first number
-                if nums[i] > nums[i+1]:
-                    return i
-            if i == size - 1: #last number
-                if nums[i] > nums[i-1]:
-                    return i
+        if size == 0:
+            return
+        return self.binary_search(nums, 0, size - 1 )
+      
+
+    def binary_search(self, nums, first, last):
+
+        mid = first + (last - first) // 2
+
+        if mid == 0:
+            if nums[mid+1] < nums[mid]:
+                return mid
+            else:
+                return self.binary_search(nums,mid+1, last)
+        
+        if mid == len(nums)-1:
+            if nums[mid-1] < nums[mid]:
+                return mid
+            else:
+                return self.binary_search(nums,first, mid)
+      
+        if nums[mid+1] < nums[mid] and nums[mid-1] < nums[mid]:
+            return mid
+
+        else:
+            if nums[mid+1] > nums[mid-1]:
+                return self.binary_search(nums,mid+1, last)
+            else:
+                return self.binary_search(nums,first, mid)
+
+        return 
             
-            if nums[i] > nums[i+1] and nums[i] > nums[i-1]: #middle
-                return i
+
+        
+
+
+
+        
+
+
+
 
         
