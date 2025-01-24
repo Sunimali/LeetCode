6d23b409615e90954a3413d228d0e1d871f74b22
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2025-01-24 10:25:09 -0600

    Time: 120 ms (30.59%) | Memory: 37.7 MB (13.21%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -6,6 +6,10 @@ class Solution:
         dic = {}  # Dictionary to store the next consecutive element
         size = len(nums)
 
+        # if size == 1:
+        #     return 1
+        
+
         for i in range(size):
             if nums[i] not in dic:
                 pre = nums[i] - 1
@@ -21,38 +25,40 @@ class Solution:
         result = 0
 
 
-        for num in dic.keys():
-            # Only process the start of a sequence
-            if num - 1 not in dic:
-                temp = 0
-                current = num
-                # Follow the "next" pointers to count the sequence length
-                while current is not None:
-                    temp += 1
-                    current = dic[current]
+        # for num in dic.keys():
+        #     # Only process the start of a sequence
+        #     if num - 1 not in dic:
+        #         temp = 0
+        #         current = num
+        #         # Follow the "next" pointers to count the sequence length
+        #         while current is not None:
+        #             temp += 1
+        #             current = dic[current]
 
-                # Update the maximum sequence length
-                result = max(result, temp)
+        #         # Update the maximum sequence length
+        #         result = max(result, temp)
 
-        # for key in dic.keys():
-        #     # Skip this key if it's already processed
-        #     if dic[key] is None:
-        #         continue
+        for key in dic.keys():
+            # Skip this key if it's already processed
+            # if dic[key] is None:
+            #     continue
+            if key - 1 in dic:
+                continue
 
-        #     temp = 0
-        #     current = key
+            temp = 0
+            current = key
 
-        #     # Traverse the sequence starting from `key`
-        #     while current in dic:
-        #         temp += 1
-        #         next_element = dic[current]
-        #         dic[current] = None  # Mark as visited
-        #         if next_element is None:
-        #             break
-        #         current = next_element
+            # Traverse the sequence starting from `key`
+            while current in dic:
+                temp += 1
+                next_element = dic[current]
+                # dic[current] = None  # Mark as visited
+                # if next_element is None:
+                #     break
+                current = next_element
 
-        #     if temp > result:
-        #         result = temp
+            if temp > result:
+                result = temp
 
         return result
 
