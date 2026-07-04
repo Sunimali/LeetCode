6aec20af47697215470fb3023cfb1646574e8615
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2026-07-04 13:28:37 -0500

    Time: 30 ms (14.24%) | Memory: 20.1 MB (64.34%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -2,12 +2,9 @@ from collections import deque
 class Solution:
     def removeDuplicates(self, s: str) -> str:
         stack = deque()
-        dupfound = False
-
         for c  in s:
             if stack:
                 if stack[-1] == c:
-                    dupfound = True
                     stack.pop()
                 else:
                     stack.append(c) 
