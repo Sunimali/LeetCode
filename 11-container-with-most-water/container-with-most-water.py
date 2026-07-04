76dc09669f992ed965312a25352f8de28c3ee612
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2026-07-03 21:53:37 -0500

    Time: 90 ms (5.17%) | Memory: 29.5 MB (88.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,29 +1,20 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        size = len(height)
-        f = 0
-        e = size -1
-        area = 0
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        cur_max = 0
+        l = 0
+        r = len(height) - 1
 
-        while(e!=f):
+        while l < len(height) and r > 0:
+            area = min(height[l], height[r]) *  (r- l)
+            cur_max  = max(cur_max, area)
 
-            min_index = 0
-            current_area = 0
-            if(height[f] <= height[e]):
-                min_index = f
-                current_area = height[min_index]* (e - f)
-                f = f + 1 #move the minimum hieght
+            if height[l] > height[r]:
+                r-=1
             else:
-                min_index = e
-                current_area = height[min_index]* (e - f)
-                e = e - 1 #move the minimum hieght
-            if(current_area> area): #check current area with perius max area value
-                area = current_area
-        return area               
-            
+                l+=1
+        
+        return cur_max
+        
+
 
         
