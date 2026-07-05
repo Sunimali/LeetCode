eb30f94ec4764934bfaaae149db28127736805e3
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2026-07-05 16:57:54 -0500

    Time: 67 ms (16.13%) | Memory: 43.5 MB (5.23%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,61 +1,39 @@
+import heapq
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        s = set(nums)
+        visited = set()
+        d = {}
+        len_max = 0
 
-        dic = {}  # Dictionary to store the next consecutive element
-        size = len(nums)
-
-     
-        for i in range(size):
-            if nums[i] not in dic:
-                pre = nums[i] - 1
-                post =  nums[i] + 1
-
-                if pre in dic:
-                    dic[pre] = nums[i]
-                if post in dic:
-                    dic[nums[i]] = post
-    
-                else:
-                    dic[nums[i]] = None #no consecutive 
-        result = 0
-
-
-        # for num in dic.keys():
-        #     # Only process the start of a sequence
-        #     if num - 1 not in dic:
-        #         temp = 0
-        #         current = num
-        #         # Follow the "next" pointers to count the sequence length
-        #         while current is not None:
-        #             temp += 1
-        #             current = dic[current]
-
-        #         # Update the maximum sequence length
-        #         result = max(result, temp)
-
-        for key in dic.keys():
-            # Skip this key if it's not a beginining #this is the key point in alogrithem
-           
-            if key - 1 in dic:
+        for num in s:
+            cur = num
+            if cur in visited:
                 continue
+            else:
+                visited.add(cur)
 
-            temp = 0
-            current = key
+            while cur in s:
+                cur += 1
+                visited.add(cur)
+                if cur in d:
+                    d[num] = d[cur]
+                    d.pop(cur)
+                    break
+            if num not in d:       
+                d[num] = cur
 
-            # Traverse the sequence starting from `key`
-            while current in dic:
-                temp += 1
-                current =  dic[current]
+        for i in d:
+            len_max = max(len_max, d[i] - i)
+        return len_max
 
-            if temp > result:
-                result = temp
 
-        return result
+    
 
+        
+            
 
             
 
-                    
             
+        
