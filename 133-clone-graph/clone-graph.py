89cd4029e99b499960b881a9a28a5f85d9856e77
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2026-07-06 12:10:11 -0500

    Time: 45 ms (83.75%) | Memory: 19.9 MB (17.69%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,43 +1,58 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        c = None
+from typing import Optional
+from collections import deque
+class Solution:
 
-        older_map = {}
+
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         
-        c = self.dfs(node, c, older_map)
-        return c
-    def dfs(self,node,c, older_map ):
-
         if node is None:
-            return c
-       
-        if node in older_map:
-            return older_map[node]
-        else:
-            clone = Node(node.val)
-            older_map[node] = clone
-   
-            for n in node.neighbors:
-                clone.neighbors.append(self.dfs(n, c,older_map ))
+            return None
+        
+        q = deque()
+        visited = set()
+        q.append(node)
+        dic = {}
+        head = None
+    
+        while q:
+            n = q.pop()
+            clone = None
             
-            return clone
-        
+            if n in visited:
+                continue
+            else:
+                if n.val not in dic:
+                    clone = Node(n.val, None)
+                    dic[n.val] = clone
+            
+                visited.add(n)
+            clone = dic[n.val]
+            if head is None:
+                head = clone
+
+            c_n = []
+            for n_i in n.neighbors:
+                if n_i.val in dic:
+                    c_n.append(dic[n_i.val])
+                else:
+                    clone_n = Node(n_i.val, None)
+                    dic[n_i.val] = clone_n
+                    c_n.append(clone_n)
+                q.append(n_i)
+            clone.neighbors = c_n
+        return head
+            
+            
+            
 
 
-        
-              
-        
-
+            
         
