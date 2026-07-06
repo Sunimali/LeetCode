c89e21ce48f8532dd07200cacd0e344247659b35
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2026-07-05 23:31:17 -0500

    Time: 10 ms (16.76%) | Memory: 19.5 MB (19.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,42 +1,33 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        #dictionary with numeric value
-        roman = {
-            "I": 1,
-            "IV":4,
-            "V": 5,
-            "IX":9,
-            "X": 10,
-            "XL": 40,
-            "L": 50,
-            "XC":90,
-            "C": 100,
-            "CD":400,
-            "D": 500,
-            "CM":900,
-            "M":1000
-        }
 
-        size = len(s)
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        main_dic = {"I": 1, "V": 5, "X":10, "L":50, "C":100, "D":500,"M":1000}
+        sp_dic = {"IV":4, "IX": 9, "XL":40, "XC":90,"CD":400, "CM":900}
+        sp_fdic = {"I","X","C"}
+
         ans = 0
-        i = size -1
-        while i >= 0:
-            if i == 0: # handle strings have odd characters count
-                ans = ans + roman.get(s[0])
-                i = i - 1
-            else:    #get two by two from end of string
-                first = s[i-1]
-                sec = s[i]
-       
-                temp = roman.get(first+sec) # check whether it is XV,IV.. 
-                if temp is None: # when it is a singal case add only sec
-                    ans = ans + roman.get(sec)
-                    i = i - 1
-                else:
-                    ans = ans + temp #get two values together
-                    i = i - 2   
-        return ans    
+        i = 0
+
+        while i < len(s):
+            print(ans)
+            if s[i] in sp_fdic:
+                next_i = i + 1
+                if  next_i < len(s):
+                    sp = s[i] + s[next_i]
+                    if sp in sp_dic:
+                        ans += sp_dic[sp]
+                        i +=2
+                        continue
+            ans+= main_dic[s[i]]
+            i+=1
+        
+        return ans
+            
+                    
+                
+                    
+
+
+
+
+        
